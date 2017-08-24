50570aa95bcdb8ac25c361a18982bf43d59cb3ff
Toru Nishimura <runqueue48gou.jp>
2017-08-24 22:18:17 +0900

    un #if 0 to use

--- sys/arch/aarch64/aarch64/aarch64_machdep.c
+++ sys/arch/aarch64/aarch64/aarch64_machdep.c
@@ -100,7 +100,6 @@ cpu_startup(void)
 vaddr_t
 cpu_proc0_init(vaddr_t ksp)
 {
-#if 0
 	struct lwp *l;
 	struct trapframe *tf;
 
@@ -110,13 +109,10 @@ cpu_proc0_init(vaddr_t ksp)
 	tf = (struct trapframe *)(ksp + USPACE) - 1;
 	memset(tf, 0, sizeof(struct trapframe));
 	memset(lwp_getpcb(l), 0, sizeof(struct pcb));
-	memset(&l->l_md, 0, sizeof(l->l_md);
+	memset(&l->l_md, 0, sizeof(l->l_md));
 	l->l_md.md_utf = l->l_md.md_ktf = tf;
 	tf->tf_spsr = SPSR_M_EL0T;
 	return (vaddr_t)tf;
-#else
-	return ksp + (1024 * 4);
-#endif
 }
 
 void
