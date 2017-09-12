09d723cbf5980a18db8fe4b006aa792f9b2c4ac3
Ryo Shimizu <ryo@nerv.org>
2017-09-13 04:38:15 +0900

    fix to compile without MULTIPROCESSOR

--- sys/arch/arm/broadcom/bcm2835_intr.c
+++ sys/arch/arm/broadcom/bcm2835_intr.c
@@ -219,10 +219,11 @@ bcm2835_icu_attach(device_t parent, device_t self, void *aux)
 #if defined(BCM2836)
 #if defined(MULTIPROCESSOR)
 	aprint_normal(": Multiprocessor");
-#endif
-
 	bcm2836mp_intr_init(curcpu());
+#else
+	pic_add(&bcm2836mp_pic[0], BCM2836_INT_BASECPUN(0));
 #endif
+#endif /* BCM2836 */
 	pic_add(sc->sc_pic, BCM2835_INT_BASE);
 
 	aprint_normal("\n");
@@ -250,8 +251,9 @@ bcm2835_irq_handler(void *frame)
 	/*
 	 * Record the pending_ipls and deliver them if we can.
 	 */
-	if ((ipl_mask & ~oldipl_mask) > oldipl_mask)
+	if ((ipl_mask & ~oldipl_mask) > oldipl_mask) {
 		pic_do_pending_ints(I32_bit, oldipl, frame);
+	}
 }
 
 static void
