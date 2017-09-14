58fc97503806d7d35baf59d6fc4e6532a665228c
Ryo Shimizu <ryo@nerv.org>
2017-09-15 05:34:55 +0900

    add support ddb backtrace. cannot trace over interrupt context yet.

--- sys/arch/aarch64/aarch64/db_trace.c
+++ sys/arch/aarch64/aarch64/db_trace.c
@@ -31,7 +31,6 @@
 __KERNEL_RCSID(0, "$NetBSD$");
 
 #include <sys/param.h>
-#include <sys/systm.h>
 
 #include <aarch64/db_machdep.h>
 
@@ -43,8 +42,55 @@ __KERNEL_RCSID(0, "$NetBSD$");
 #include <ddb/db_extern.h>
 #include <ddb/db_interface.h>
 
+
+/*
+ *  fp[0]  saved fp(x29) value
+ *  fp[1]  saved lr(x30) value
+ */
+
+#define MAXBACKTRACE	64	/* against loop */
+
 void
 db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
     const char *modif, void (*pr)(const char *, ...))
 {
+	uint64_t pc, sp, lr, lastlr;
+	uint64_t frame;
+	int maxbacktrace;
+
+//	db_printf("%s: addr=%016llx have_addr=%d, count=%lld, modif=%s\n",
+//	    __func__, addr, have_addr, count, modif);
+
+	if (!have_addr) {
+		frame = DDB_REGS->tf_reg[29];	/* fp = x29 */
+		sp = DDB_REGS->tf_sp;
+		lr = DDB_REGS->tf_lr;
+	} else {
+		db_printf("%s: %016llx: not supported now\n", __func__, addr);
+		return;
+	}
+
+	for (maxbacktrace = MAXBACKTRACE;
+	    (maxbacktrace > 0) && (frame != 0);) {
+		db_expr_t offset;
+		db_sym_t sym;
+		const char *name;
+
+		pc = lr - 4;
+
+		sym = db_search_symbol(pc, DB_STGY_ANY, &offset);
+		if (sym != DB_SYM_NULL) {
+			db_symbol_values(sym, &name, NULL);
+
+			(*pr)("%s() at ", name);
+		}
+		db_printsym(pc, DB_STGY_PROC, pr);
+//		(*pr)(" (%016llx)", pc);
+		(*pr)("\n", pc);
+
+		lastlr = lr;
+		db_read_bytes(frame + 8, sizeof(db_addr_t), (char *)&lr);
+		db_read_bytes(frame, sizeof(db_addr_t), (char *)&frame);
+	}
+
 }
