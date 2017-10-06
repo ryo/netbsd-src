8b99c96d4fb58962318ea3b2608074263ff05c16
Ryo Shimizu <ryo@nerv.org>
2017-10-06 21:41:47 +0900

    vtophys() once for each page

--- sys/arch/aarch64/aarch64/db_interface.c
+++ sys/arch/aarch64/aarch64/db_interface.c
@@ -51,15 +51,19 @@ __KERNEL_RCSID(0, "$NetBSD$");
 void
 db_read_bytes(vaddr_t addr, size_t size, char *data)
 {
+	vaddr_t lastaddr;
 	const char *src;
 
+	lastaddr = trunc_page(addr) - PAGE_SIZE;
 	for (src = (const char *)addr; size > 0;) {
 		uintptr_t tmp;
 
-		if (vtophys((vaddr_t)src) == VTOPHYS_FAILED) {
+		if ((lastaddr != trunc_page((vaddr_t)src)) &&
+		    vtophys((vaddr_t)src) == VTOPHYS_FAILED) {
 			db_printf("address %p is invalid\n", src);
 			return;
 		}
+		lastaddr = trunc_page((vaddr_t)src);
 
 		tmp = (uintptr_t)src | (uintptr_t)data;
 		if ((size >= 8) && ((tmp & 7) == 0)) {
@@ -87,16 +91,20 @@ db_read_bytes(vaddr_t addr, size_t size, char *data)
 void
 db_write_bytes(vaddr_t addr, size_t size, const char *data)
 {
+	vaddr_t lastaddr;
 	char *dst;
 
 	//XXXAARCH64: check text segment
+	lastaddr = trunc_page(addr) - PAGE_SIZE;
 	for (dst = (char *)addr; size > 0;) {
 		uintptr_t tmp;
 
-		if (vtophys((vaddr_t)dst) == VTOPHYS_FAILED) {
+		if ((lastaddr != trunc_page((vaddr_t)dst)) &&
+		    (vtophys((vaddr_t)dst) == VTOPHYS_FAILED)) {
 			db_printf("address %p is invalid\n", dst);
 			return;
 		}
+		lastaddr = trunc_page((vaddr_t)dst);
 
 		tmp = (uintptr_t)dst | (uintptr_t)data;
 		if ((size >= 8) && ((tmp & 7) == 0)) {
