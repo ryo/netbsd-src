ceb26fc9b9b9c8d8d021a1dd00ef7eee475d1004
Ryo Shimizu <ryo@nerv.org>
2017-10-10 03:12:45 +0900

    * don't show EL0 trapframe of kernel thread * add support bt/t * cleanup

--- sys/arch/aarch64/aarch64/db_trace.c
+++ sys/arch/aarch64/aarch64/db_trace.c
@@ -42,6 +42,7 @@ __KERNEL_RCSID(0, "$NetBSD$");
 #include <ddb/db_output.h>
 #include <ddb/db_variables.h>
 #include <ddb/db_sym.h>
+#include <ddb/db_proc.h>
 #include <ddb/db_extern.h>
 #include <ddb/db_interface.h>
 
@@ -54,29 +55,29 @@ __KERNEL_RCSID(0, "$NetBSD$");
 	((VM_MIN_KERNEL_ADDRESS <= (addr)) && ((addr) < VM_MAX_KERNEL_ADDRESS))
 
 static void
-pr_traceaddr(const char *prefix, uint64_t frame, uint64_t pc, const char **name,
+pr_traceaddr(const char *prefix, uint64_t frame, uint64_t pc,
     void (*pr)(const char *, ...))
 {
 	db_expr_t offset;
 	db_sym_t sym;
+	const char *name;
 
 	sym = db_search_symbol(pc, DB_STGY_ANY, &offset);
 	if (sym != DB_SYM_NULL) {
-		db_symbol_values(sym, name, NULL);
+		db_symbol_values(sym, &name, NULL);
+		(*pr)("%s %016llx %s() at %016llx ", prefix, frame, name, pc);
+		db_printsym(pc, DB_STGY_PROC, pr);
+		(*pr)("\n");
 	} else {
-		*name = "?";
+		(*pr)("%s %016llx ?() at %016llx\n", prefix, frame, pc);
 	}
-	(*pr)("%s %016llx %s() at %016llx ", prefix, frame, *name, pc);
-	db_printsym(pc, DB_STGY_PROC, pr);
-	(*pr)("\n");
 }
 
 void
 db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
     const char *modif, void (*pr)(const char *, ...))
 {
-	struct lwp *l;
-	uint64_t lr, fp, lastfp;
+	uint64_t lr, fp, lastlr, lastfp;
 	struct trapframe *tf;
 	bool trace_user = false;
 	bool trace_thread = false;
@@ -98,9 +99,9 @@ db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
 			trace_user = true;
 			break;
 		default:
-			db_printf("usage: bt[/ul] [frame-address][,count]\n");
-			db_printf("       bt/t[l] [pid][,count]\n");
-			db_printf("       bt/a[ul] [lwpaddr][,count]\n");
+			pr("usage: bt[/ul] [frame-address][,count]\n");
+			pr("       bt/t[l] [pid][,count]\n");
+			pr("       bt/a[ul] [lwpaddr][,count]\n");
 			return;
 		}
 	}
@@ -111,29 +112,49 @@ db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
 		} else if (trace_thread) {
 			addr = curlwp->l_proc->p_pid;
 		} else {
-			addr = &DDB_REGS->tf_reg[29];
+			addr = &DDB_REGS->tf_reg[29];	/* &reg[29]={fp,lr} */
 		}
 	}
 
+	if (trace_thread) {
+		proc_t *pp, p;
+
+		if ((pp = db_proc_find((pid_t)addr)) == 0) {
+			(*pr)("trace: pid %d: not found\n", (int)addr);
+			return;
+		}
+		db_read_bytes((db_addr_t)pp, sizeof(p), (char *)&p);
+		addr = (db_addr_t)p.p_lwps.lh_first;
+		trace_thread = false;
+		trace_lwp = true;
+	}
+
 	if (trace_lwp) {
-		l = (struct lwp *)addr;
-		tf = 0;
-		db_read_bytes(&l->l_md.md_ktf, sizeof(tf), (char *)&tf);
-		fp = &tf->tf_reg[29];
-		db_printf("trace lwp %p\n", l);
-	} else if (trace_thread) {
-		db_printf("bt/t: not implemented\n");
-		return;
+		proc_t p;
+		struct lwp l;
+
+		db_read_bytes(addr, sizeof(l), (char *)&l);
+		db_read_bytes((db_addr_t)l.l_proc, sizeof(p), (char *)&p);
+
+		if (addr == (db_addr_t)curlwp) {
+			fp = &DDB_REGS->tf_reg[29];	/* &reg[29]={fp,lr} */
+			(*pr)("trace: pid %d lid %d (curlwp) at fp %016lx\n",
+			    p.p_pid, l.l_lid, fp);
+		} else {
+			tf = l.l_md.md_ktf;
+			db_read_bytes(&tf->tf_reg[29], sizeof(fp), (char *)&fp);
+			(*pr)("trace: pid %d lid %d at tf %016lx\n",
+			    p.p_pid, l.l_lid, tf);
+		}
 	} else {
 		fp = addr;
-		db_printf("trace fp %016llx\n", fp);
+		pr("trace fp %016llx\n", fp);
 	}
 
 	if (count > MAXBACKTRACE)
 		count = MAXBACKTRACE;
 
 	for (; (count > 0) && (fp != 0); count--) {
-		const char *name = NULL;
 
 		lastfp = fp;
 		/*
@@ -158,12 +179,18 @@ db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
 
 			tf = (struct trapframe *)(lastfp + 16);
 
-			pr_traceaddr("tf", tf, lr - 4, &name, pr);
-
 			lastfp = tf;
+			lastlr = lr;
 			lr = fp = 0;
 			db_read_bytes(&tf->tf_pc, sizeof(lr), (char *)&lr);
 			db_read_bytes(&tf->tf_reg[29] + 0, sizeof(fp), (char *)&fp);
+
+			/* no need to display the frame of el0_trap of kernel thread */
+			if (((char *)(lastlr - 4) == (char *)el0_trap) && (lr == 0))
+				break;
+
+
+			pr_traceaddr("tf", tf, lastlr - 4, pr);
 
 			if (lr == 0)
 				break;
@@ -175,10 +202,10 @@ db_stack_trace_print(db_expr_t addr, bool have_addr, db_expr_t count,
 			if (!trace_user && IN_USER_VM_ADDRESS(lr))
 				break;
 
-			pr_traceaddr("fp", fp, lr, &name, pr);
+			pr_traceaddr("fp", fp, lr, pr);
 
 		} else {
-			pr_traceaddr("fp", fp, lr - 4, &name, pr);
+			pr_traceaddr("fp", fp, lr - 4, pr);
 		}
 	}
 }
