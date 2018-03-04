f4a5ea92dd56224d6ed3c6ab22187f4ca624faf5
Nick Hudson <skrll@netbsd.org>
2018-03-04 14:47:54 +0000

    Remove some XXXs

--- sys/arch/evbarm/include/lock.h
+++ sys/arch/evbarm/include/lock.h
@@ -1,6 +1,5 @@
 /*	$NetBSD: lock.h,v 1.1 2001/11/25 15:56:05 thorpej Exp $	*/
 
-// XXXNH arm/lock.h has v8 stuff already but is a bit of a mess atm
 #ifdef __aarch64__
 #include <aarch64/lock.h>
 #else
